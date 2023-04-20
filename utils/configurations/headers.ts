22563f50af7266cbfda0b7c9e7634c69e852d721
Raghav <raghav.kanwal@unicommerce.com>
2023-04-20 14:24:18 +0530

    remove x-api-key

--- utils/configurations/headers.ts
+++ utils/configurations/headers.ts
@@ -3,10 +3,10 @@ export function getHeaders(method: string): Headers {
   headers.append('Content-type', 'application/json')
 
   if (method === 'POST') {
-    headers.append('x-api-key', 'test1-1234')
+    // headers.append('x-api-key', 'test1-1234')
   } else {
     //uc_sales-wXDbwosW6tsi2SuLM3AI
-    headers.append('X-API-KEY', 'test1-1234')
+    // headers.append('X-API-KEY', 'test1-1234')
   }
   return headers
 }
